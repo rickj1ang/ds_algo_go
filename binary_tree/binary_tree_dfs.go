79fc34932597f4f853ded77d6f90586f47218d43
rickj1ang <rick0j1ang@gmail.com>
2024-12-07 12:49:47 +0800

    DFS and test

--- binary_tree/binary_tree_dfs.go
+++ binary_tree/binary_tree_dfs.go
@@ -16,16 +16,16 @@ func (res *dfsRes) inOrder(node *TreeNode) {
 	if node == nil {
 		return
 	}
-	res.preOrder(node.Left)
+	res.inOrder(node.Left)
 	*res = append(*res, node.Val)
-	res.preOrder(node.Right)
+	res.inOrder(node.Right)
 }
 
 func (res *dfsRes) postOrder(node *TreeNode) {
 	if node == nil {
 		return
 	}
-	res.preOrder(node.Left)
-	res.preOrder(node.Right)
+	res.postOrder(node.Left)
+	res.postOrder(node.Right)
 	*res = append(*res, node.Val)
 }
--- binary_tree/binary_tree_dfs_test.go
+++ binary_tree/binary_tree_dfs_test.go
@@ -1,1 +1,36 @@
 package binarytree
+
+import "testing"
+
+func TestDFS(t *testing.T) {
+	/*
+	          7
+	        /  \
+	      4     10
+	    /  \    / \
+	   30  27  31 18
+	*/
+	root := NewTreeNode(7)
+	root.Left = NewTreeNode(4)
+	root.Right = NewTreeNode(10)
+	root.Left.Left = NewTreeNode(30)
+	root.Left.Right = NewTreeNode(27)
+	root.Right.Left = NewTreeNode(31)
+	root.Right.Right = NewTreeNode(18)
+
+	var preRes dfsRes
+	preRes.preOrder(root)
+
+	var inRes dfsRes
+	inRes.inOrder(root)
+
+	var postRes dfsRes
+	postRes.postOrder(root)
+
+	t.Log("pre:\n")
+	t.Log(preRes)
+	t.Log("in:\n")
+	t.Log(inRes)
+	t.Log("post:\n")
+	t.Log(postRes)
+}
