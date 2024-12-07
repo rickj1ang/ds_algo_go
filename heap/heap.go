463a2d1ceaac7a4cee6d73e0ab7d7b02eebff616
rickj1ang <rick0j1ang@gmail.com>
2024-12-07 08:51:44 +0800

    Maxheap and it's test

--- heap/heap.go
+++ heap/heap.go
@@ -1,6 +1,9 @@
 package heap
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
 // We need to impl the interface of IntSlice(Less, Len, Swap) and Push(), Pop()
 // to impl a MinHeap, but this code is a easy way to impl MinHeap with Golang
@@ -29,4 +32,104 @@ func (m *MinHeap) Top() any {
 
 func Constructor() MinHeap {
 	return MinHeap{}
+}
+
+// own maxHeap for sake of learning make no sense
+// impl it by yourself when you need to use them
+type maxHeap struct {
+	data []any
+}
+
+func NewMaxHeap() maxHeap {
+	return maxHeap{
+		data: make([]any, 0),
+	}
+}
+
+/*
+				5       value:[5, 3, 7, 2, 6, 9, 4]
+			/  \ 			index:[0, 1, 2, 3, 4, 5, 6]
+		 3    7
+	  / \  / \
+	 2  6  9  4
+*/
+func (m *maxHeap) left(i int) int {
+	return 2*i + 1
+}
+func (m *maxHeap) right(i int) int {
+	return 2*i + 2
+}
+func (m *maxHeap) parent(i int) int {
+	return (i - 1) / 2
+}
+
+func (m *maxHeap) peek() any {
+	return m.data[0]
+}
+
+func (m *maxHeap) push(val any) {
+	m.data = append(m.data, val)
+	m.siftUp(len(m.data) - 1)
+}
+
+func (m *maxHeap) siftUp(i int) {
+	for true {
+		p := m.parent(i)
+		if p < 0 || m.data[i].(int) <= m.data[p].(int) {
+			break
+		}
+
+		m.swap(i, p)
+		i = p
+	}
+}
+
+func (m *maxHeap) swap(i, j int) {
+	m.data[i], m.data[j] = m.data[j], m.data[i]
+}
+
+func (m *maxHeap) pop() any {
+	if m.isEmpty() {
+		fmt.Println("error")
+		return nil
+	}
+	m.swap(0, len(m.data)-1)
+	val := m.data[len(m.data)-1]
+	m.data = m.data[:len(m.data)-1]
+
+	m.siftDown(0)
+
+	return val
+}
+
+func (m *maxHeap) siftDown(i int) {
+	for true {
+		leftSon := m.left(i)
+		rightSon := m.right(i)
+		maxIndex := i
+
+		if leftSon < m.size() && m.data[leftSon].(int) > m.data[maxIndex].(int) {
+			maxIndex = leftSon
+		}
+		if rightSon < m.size() && m.data[rightSon].(int) > m.data[maxIndex].(int) {
+			maxIndex = rightSon
+		}
+		if maxIndex == i {
+			break
+		}
+		m.swap(i, maxIndex)
+
+		i = maxIndex
+	}
+}
+
+func (m *maxHeap) size() int {
+	return len(m.data)
+}
+
+func (m *maxHeap) isEmpty() bool {
+	if len(m.data) > 0 {
+		return false
+	}
+	return true
 }
--- heap/heap_test.go
+++ heap/heap_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestMaxHeap(t *testing.T) {
+func TestMinHeap(t *testing.T) {
 	minH := Constructor()
 	heap.Push(&minH, 4)
 	heap.Push(&minH, 2)
@@ -15,6 +15,7 @@ func TestMaxHeap(t *testing.T) {
 	if min := heap.Pop(&minH); min != 1 {
 		t.Errorf("Expect: 1; Pop out: %d", min)
 	}
+
 	if min := heap.Pop(&minH); min != 2 {
 		t.Errorf("Expect: 2; Pop out: %d", min)
 	}
@@ -22,5 +23,24 @@ func TestMaxHeap(t *testing.T) {
 	if top := minH.Top(); top != 4 {
 		t.Errorf("Expect: 4; Pop out: %d", top)
 	}
+}
 
+func TestMaxHeap(t *testing.T) {
+	// new push pop peek
+	maxH := NewMaxHeap()
+	maxH.push(6)
+	maxH.push(8)
+	maxH.push(5)
+	maxH.push(1)
+	maxH.push(3)
+	maxH.push(7)
+	if maxH.peek() != 8 {
+		t.Errorf("Peek:: expect: 8; actual: %d", maxH.peek())
+	}
+	if out := maxH.pop(); out != 8 {
+		t.Errorf("Pop:: expect: 8; actual: %d", out)
+	}
+	if maxH.peek() != 7 {
+		t.Errorf("Peek:: expect: 7; actual: %d", maxH.peek())
+	}
 }
