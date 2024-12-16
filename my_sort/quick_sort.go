583f47135db6c98c3c5bb9f32d0a6b94b12e7612
rickj1ang <rick0j1ang@gmail.com>
2024-12-16 08:30:31 +0800

    impl partion part

--- my_sort/quick_sort.go
+++ my_sort/quick_sort.go
@@ -1,1 +1,17 @@
 package mysort
+
+// receive slice left and right boundary of the nums we want to sort
+// return the index of base num AKA middle num
+func partion(nums []int, left, right int) int {
+	i, j := left, right
+	for i < j {
+		for i < j && nums[j] <= nums[left] {
+			j--
+		}
+		for i < j && nums[i] >= nums[right] {
+			i--
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+	return i
+}
